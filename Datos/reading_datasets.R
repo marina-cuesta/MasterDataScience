78479d58143c8b78102da646fd4731921ed2d8b4
marina.cuesta <marinacuesta.st@gmail.com>
2023-01-17 14:46:19 +0100

    Classification data sets

---
+++ Datos/reading_datasets.R
@@ -0,0 +1,39 @@
+############################
+######   WINE DATA    ######
+############################
+
+# https://rdrr.io/cran/HDclassif/man/wine.html
+
+#install.packages(HDclassif)
+library(HDclassif)
+
+data(wine)
+data=wine
+
+
+####################################
+######   BREASTCANCER DATA    ######
+####################################
+
+# https://rdrr.io/cran/mlbench/man/BreastCancer.html
+
+#install.packages(mlbench)
+library(mlbench)
+
+data(BreastCancer)
+data=BreastCancer
+
+
+
+############################
+######   PIMA DATA    ######
+############################
+
+# https://rdrr.io/cran/pdp/man/pima.html
+
+#install.packages(pdp)
+library(pdp)
+
+data(pima)
+data = pima
+
